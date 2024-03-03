712a32616614684ca97053c4024cacf1bf1e4e49
MilosJovic23 <milos.mj992@gmail.com>
2024-03-03 11:49:11 +0100

    * homework assignment

--- src/Components/PayPal.jsx
+++ src/Components/PayPal.jsx
@@ -1,12 +1,9 @@
 import {useContext} from "react";
 import {ConvertedAmountContext} from "../App";
 
-
 const PayPal=()=>{
     const amount=useContext(ConvertedAmountContext)
 
     return(<p>PayPal: {amount} RSD</p>)
 }
-
-
 export default PayPal;
