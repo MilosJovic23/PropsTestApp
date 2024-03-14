66746ca4ea017e5f33bf779ca6d04bdb50db07df
MilosJovic23 <milos.mj992@gmail.com>
2024-03-14 11:05:29 +0100

    * homework assignment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Payment from "./Components/Payment";
-import {createContext, useState} from "react";
+import {createContext, useEffect, useReducer, useState} from "react";
+import {initialUserState, LoadUserState, UserReducer} from "./Reducers/User";
 
 
 
 export const ConvertedAmountContext=createContext(0)
 const App= ()=>{
-    // const currency="USD";
+    const [userState,dispatch]=useReducer(UserReducer,LoadUserState())
+
     const [amount,setAmount]=useState(0);
     const [currencyIndex,setCurrencyIndex]=useState(108)
     const [convertedAmount,setConvertedAmount]=useState(0)
+
+
     const  updateCurrency=(e)=>{
         setConvertedAmount(amount*currencyIndex)
     };
@@ -18,22 +22,44 @@ const App= ()=>{
     const updateCurrencyIndex=(e)=>{
         setCurrencyIndex(e.currentTarget.value)
     }
+    const saveUser =()=>{
+       if( userState.userName === null || userState.userName.trim()==='' ||userState.money === null || isNaN(userState.money) ){
+           return;
+       }
+       dispatch({type:"SET_USER_CREATED" ,payload:true});
 
+    }
+
+    useEffect(()=>{
+        if(userState.isUserCreated){
+            localStorage.setItem("userState",JSON.stringify(userState))
+        }
+    },[userState])
   return(
       <>
          <ConvertedAmountContext.Provider value={convertedAmount}>
              <Payment/>
          </ConvertedAmountContext.Provider>
-          <form>
+
               <input type="number" onInput={e => updateAmount(e)}/>
-              <select onChange={e=>updateCurrencyIndex(e)}>
+          { !userState.isUserCreated && <form>
+              <input placeholder="enter your username" type="text"
+              onInput={e => dispatch({type:"SET_USERNAME", payload:e.target.value})}/>
+              <input placeholder="enter your money"
+              onInput={e => dispatch({type:"SET_MONEY", payload:e.target.value})}/>
+    <button type="button" onClick={saveUser}>Kreiraj Korisnika</button>
+</form>}
+
+
+
+              <select onChange={e => updateCurrencyIndex(e)}>
                   <option value={108}>USD &#36;</option>
                   <option value={117.2}>EUR &euro;</option>
                   <option value={136.6}>GBP &#163;</option>
                   <option value={1.2}>RBL &#8381;</option>
               </select>
               <button type="button" onClick={updateCurrency}>Change currency</button>
-          </form>
+
 
       </>
 
